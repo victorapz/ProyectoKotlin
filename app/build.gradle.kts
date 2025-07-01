3c32f92540d1b72fa3bb347a4b5a72440fe63d8d
Víctor Pérez <victor.perez2101@alumnos.ubiobio.cl>
2025-06-30 22:27:13 -0400

    feat: create LoginActivity and  initial activity_login.xml structure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,9 @@ android {
             )
         }
     }
+    buildFeatures {
+        dataBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -45,4 +48,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    //Material design
+    implementation("com.google.android.material:material:1.12.0")
+    //ViewModel
+
+
+
 }
